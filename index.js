9ea1b9c9769788ab6400d71202ca257540b2ad73
sikang <updream4@naver.com>
2020-07-01 20:18:41 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+
+const config = require('./config/key')
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kwak:kwak@cluster0.frwj0.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true,useFindAndModify:false
 }).then(() => console.log('connect complete'))
 .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
+app.post('/register',(req,res)=> {
+    
+    const user = new User(req.body)
+
+    user.save((err,userInfo)=>{
+
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({
+            sucess:true
+        })
+    })
+
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
