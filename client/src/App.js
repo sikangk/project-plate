d0eeeaa26cd7b0fd3273c3eb819fe18354ec3b1c
sikang <updream4@naver.com>
2020-07-13 15:44:22 +0900

    마무리

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import {
 import LandingPage from './components/views/LandingPage/LandingPage'
 import LoginPage from './components/views/LoginPage/LoginPage' ;
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import Auth from '../src/hoc/auth';
 
 function App() {
   return (
@@ -21,12 +22,14 @@ function App() {
           you have multiple routes, but you want only one
           of them to render at a time
         */}
+        
+        
         <Switch>
-          <Route exact path="/" component={LandingPage}/>
+          <Route exact path="/" component={Auth(LandingPage, null)}/>
             
-          <Route exact path="/login" component={LoginPage}/>
+          <Route exact path="/login" component={Auth(LoginPage, false)}/>
             
-          <Route exact path="/register" component={RegisterPage}/>
+          <Route exact path="/register" component={Auth(RegisterPage,false)}/>
             
         </Switch>
       </div>
@@ -35,6 +38,6 @@ function App() {
 }
 
 
-
+//admin 유저는 true
 
 export default App;
